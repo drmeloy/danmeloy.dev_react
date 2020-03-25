337ea69aebf0aec7afff40d3d780a4ffc366ee74
drmeloy <drmeloy@gmail.com>
2020-03-25 15:56:08 -0700

    adding Background section

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import Landing from './Landing/Landing';
 import Nav from './Nav/Nav';
 import About from './About/About';
 import Work from './Work/Work';
+import Background from './Background/Background';
 
 export default function App() {
   return (
@@ -11,6 +12,7 @@ export default function App() {
       <Landing />
       <About />
       <Work />
+      <Background />
     </>
   );
 }
