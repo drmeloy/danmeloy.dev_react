15d28d884dd807653b33719bed7dfbe2705fc5a2
drmeloy <drmeloy@gmail.com>
2020-03-26 14:29:20 -0700

    phone mobile responsiveness on background section

--- src/components/Background/Background.jsx
+++ src/components/Background/Background.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from './Background.css';
+import mobile from './MobileBackground.css';
 import { Element } from 'react-scroll';
 
 export default function Background(){
@@ -17,7 +18,26 @@ export default function Background(){
           <p>I&apos;ve opened two successful businesses abroad in Quito, Ecuador. Being a co-owner and founder of <a href='https://communityhostel.com/' target='_blank' rel='noopener noreferrer'>Community Hostel</a> and <a href='http://bandidobrewing.com/en/' target='_blank' rel='noopener noreferrer'>Bandido Brewing</a> taught me how to take ideas and turn them into reality. The challenges of opening two businesses in a foreign country taught me the value of adaptability. With a mindset of adaptability, any problem is solvable, any issue or interruption is merely a part of the process. To this day my adaptability remains one of my greatest strengths.</p>
           <p>I&apos;ve been a leader and facilitator for a wilderness non-profit. I led backcountry wilderness trips for adults with physical and developmental disabilities and facilitated outdoor experiential education events for under-served student communities. By helping people connect with the outdoors I was helping them build their confidence and realize that they can do anything they set their minds to, regardless of personal background or ability level.</p>
         </div>
-        <p className={styles.Closer}>Now, as a developer and engineer, I hope to use technology to apply the lessons I&apos;ve learned throughout my life. I aim to solve problems that can empower others.</p>
+        <p className={styles.Closer}>Now, as a developer and engineer, I use technology to apply the lessons I&apos;ve learned throughout my life. I aim to solve problems that empower others.</p>
+      </section>
+      <section className={mobile.Background}>
+        <p className={mobile.Title}>A bit about my background...</p>
+        <p className={mobile.Blurb}>Expand the sections below to learn about my approach to life and work:</p>
+        <div className={mobile.Excerpts}>
+          <details className={mobile.Detail}>
+            <summary>Perspectives-based approach</summary>
+            <p className={mobile.Excerpt}>As a student of Anthropology, I learned how perspectives often vary and hugely differentiate the way we understand the world. Throughout my life I strive to keep a perspectives-based approach to problem solving and relationship building, believing that the first step to success is understanding others as best we can.</p>
+          </details>
+          <details className={mobile.Detail}>
+            <summary>International entrepreneur</summary>
+            <p className={mobile.Excerpt}>I&apos;ve opened two successful businesses abroad in Quito, Ecuador. Being a co-owner and founder of <a href='https://communityhostel.com/' target='_blank' rel='noopener noreferrer'>Community Hostel</a> and <a href='http://bandidobrewing.com/en/' target='_blank' rel='noopener noreferrer'>Bandido Brewing</a> taught me how to take ideas and turn them into reality. The challenges of opening two businesses in a foreign country taught me the value of adaptability. With a mindset of adaptability, any problem is solvable, any issue or interruption is merely a part of the process. To this day my adaptability remains one of my greatest strengths.</p>
+          </details>
+          <details className={mobile.Detail}>
+            <summary>Leadership and empowering others</summary>
+            <p className={mobile.Excerpt}>I&apos;ve been a leader and facilitator for a wilderness non-profit. I led backcountry wilderness trips for adults with physical and developmental disabilities and facilitated outdoor experiential education events for under-served student communities. By helping people connect with the outdoors I was helping them build their confidence and realize that they can do anything they set their minds to, regardless of personal background or ability level.</p>
+          </details>
+        </div>
+        <p className={mobile.Closer}>Now, as a developer and engineer, I use technology to apply the lessons I&apos;ve learned throughout my life. I aim to solve problems that empower others.</p>
       </section>
     </Element>
   );
